3897f4a25d9915df64d2e5623b50b2bf21a31a21
Carlos Villa <cvillad@uninorte.edu.co>
2021-03-18 16:40:12 -0500

    add stripe payment for users

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,5 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "../credit_card_form"
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -11,7 +11,7 @@ class Payment < ApplicationRecord
   end
 
   def process_payment
-    customer = Stripe::Customer.create email: email, card: token 
+    customer = Stripe::Customer.create email: email, source: 'tok_visa'
     Stripe::Charge.create customer: customer.id, 
                           amount: 1000,
                           description: "Premium",
