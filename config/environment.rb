31885b04e554873780f720093e8452cca7505d57
Carlos Villa <cvillad@uninorte.edu.co>
2021-03-18 10:09:00 -0500

    add mailtrap

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,19 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+require 'rubygems' if RUBY_VERSION < '1.9'
+require 'rest-client'
+require 'json'
 
+response = RestClient.get "https://mailtrap.io/api/v1/inboxes.json?api_token=#{ENV['MAILTRAP_API_TOKEN']}"
+first_inbox = JSON.parse(response)[0] # get first inbox
+
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port: 587,
-  authentication: :plain,
-  user_name: 'apikey',
-  password: ENV['SENDGRID_PASSWORD'],
-  domain: 'heroku.com',
-  enable_starttls_auto: true
+  :user_name => first_inbox['username'],
+  :password => first_inbox['password'],
+  :address => first_inbox['domain'],
+  :domain => first_inbox['domain'],
+  :port => first_inbox['smtp_ports'][0],
+  :authentication => :plain
 }
